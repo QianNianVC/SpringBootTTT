df41318132b3581723633885743f883b4f7da291
QianNianVC <m2075725760@163.com>
2025-12-24 11:24:41 +0800

    Initial commit

---
+++ src/main/java/com/study/springbootttt/SpringBootTttApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTttApplication.class, args);
+    }
+
+}
